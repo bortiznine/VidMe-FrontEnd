bbfffe785b8fb1c611d08537fb107e5097505e62
Brian Ortiz <bortiznine@gmail.com>
2021-05-14 15:36:30 -0500

    Edited app module file to add httpclient and formmodule along with routingmodule

--- VidMe-FrontEnd/src/app/app.module.ts
+++ VidMe-FrontEnd/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { VidmelibraryComponent } from './vidmelibrary/vidmelibrary.component';
 import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
