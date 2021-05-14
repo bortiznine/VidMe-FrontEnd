1fe483107471406d0256622c26171ba9720e7b4d
Brian Ortiz <bortiznine@gmail.com>
2021-05-14 15:14:10 -0500

    Added approuting.module as initial project did not start with one

---
+++ VidMe-FrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CategoriesComponent } from './vidmelibrary/vidmelibrary.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'vidmelibrary',
+    component: CategoriesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
