8d10d6316760feaf855719605b2aa89315113436
Brian Ortiz <bortiznine@gmail.com>
2021-05-18 10:07:16 -0500

    Added ngif conditionals to navbar for being logged in or logged out. Edited app html to have a container

--- VidMe-FrontEnd/src/app/navbar/navbar.component.ts
+++ VidMe-FrontEnd/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../services/user/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private userService: UserService) { }
+currentUser: any;
   ngOnInit(): void {
+    this.userService.searchSubject.subscribe(currentUser => {
+      this.currentUser = currentUser;
+      console.log(currentUser);
+    });
   }
 
 }
